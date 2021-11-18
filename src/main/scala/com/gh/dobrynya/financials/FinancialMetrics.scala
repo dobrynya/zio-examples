65663be4314b9469fc53b964e596a1656cdd35e0
Dmitry Dobrynin <dobrynya@inbox.ru>
2021-11-19 01:34:38 +0300

    Refactored: filter has been replaced by just usual predicate

--- src/main/scala/com/gh/dobrynya/financials/FinancialMetrics.scala
+++ src/main/scala/com/gh/dobrynya/financials/FinancialMetrics.scala
@@ -10,6 +10,11 @@ import java.util.Locale
 import scala.collection.immutable.SortedSet
 import scala.util.Try
 import java.time.LocalDate
+import java.time.Month.NOVEMBER
+
+extension (f: Instrument => Boolean)
+  def and(another: Instrument => Boolean) = (i: Instrument) => f(i) && another(i)
+  def or(another: Instrument => Boolean) = (i: Instrument) => f(i) && another(i)
 
 case class Instrument(name: String, date: LocalDate, price: Double)
 
@@ -19,13 +24,21 @@ object Instrument:
     Instrument(p(0), LocalDate.parse(p(1), DateTimeFormatter.ofPattern("dd-MMM-uuuu", Locale.US)), p(2).toDouble)
   }.toOption
 
+def name(name: String) = (i: Instrument) => i.name == name
+
+def date(from: LocalDate = LocalDate.MIN, to: LocalDate = LocalDate.MAX) =
+  (i: Instrument) => i.date.compareTo(from) >= 0 && i.date.compareTo(to) <= 0
+
+val workingDays: (Instrument => Boolean) =
+  (i: Instrument) => i.date.getDayOfWeek != DayOfWeek.SUNDAY && i.date.getDayOfWeek != DayOfWeek.SATURDAY
+
 object FinancialMetrics extends App:
   val raw =
     ZStream.fromFile(Paths.get("instruments.txt")) >>> ZTransducer.utfDecode >>> ZTransducer.splitLines
 
-  val instruments =
+  val instruments: ZStream[Blocking, Throwable, Instrument] =
     raw.map(Instrument.parse).collectSome
-      .filter(Date(to = LocalDate.of(2014, Month.NOVEMBER, 19)) and WorkingDays)
+      .filter(date(to = LocalDate.of(2014, NOVEMBER, 19)) and workingDays)
 
   val mean: Sink[Nothing, Instrument, Nothing, Double] = ZSink.foldLeft((0.0, 0)) { (s, elem: Instrument) =>
     (s._1 + elem.price, s._2 + 1)
@@ -37,17 +50,16 @@ object FinancialMetrics extends App:
       if (result.size > n) result.drop(1) else result
     }.map(_.map(_.price).sum)
 
-  def makeCalculation[R](hub: Hub[Instrument], sink: Sink[Nothing, Instrument, Nothing, R], filter: Filter): URIO[Any, Fiber.Runtime[Nothing, R]] =
+  def makeCalculation[R](hub: Hub[Instrument], sink: Sink[Nothing, Instrument, Nothing, R], filter: Instrument => Boolean): URIO[Any, Fiber.Runtime[Nothing, R]] =
     ZStream.fromHub(hub).filter(filter).run(sink).fork
 
   override def run(args: List[String]) =
     (for {
       hub <- ZHub.unbounded[Instrument]
-      i1mean <- makeCalculation(hub, mean, Name("INSTRUMENT1"))
-      i2mean4november2014 <- makeCalculation(hub, mean, Name("INSTRUMENT2") and
-            Date(LocalDate.of(2014, Month.NOVEMBER, 1),
-              LocalDate.of(2014, Month.NOVEMBER, 30)))
-      i3sumLast <- makeCalculation(hub, sumLast(10), Name("INSTRUMENT3"))
+      i1mean <- makeCalculation(hub, mean, name("INSTRUMENT1"))
+      i2mean4november2014 <- makeCalculation(hub, mean, name("INSTRUMENT2") and
+        date(LocalDate.of(2014, NOVEMBER, 1), LocalDate.of(2014, NOVEMBER, 30)))
+      i3sumLast <- makeCalculation(hub, sumLast(10), name("INSTRUMENT3"))
       _ <- instruments.foreach(hub.publish)
       _ <- console.putStrLn("Completed reading file")
       _ <- hub.shutdown
@@ -55,21 +67,3 @@ object FinancialMetrics extends App:
       _ <- i2mean4november2014.join.flatMap(d => console.putStrLn(s"Mean for INSTRUMENT2 for November 2014 is $d"))
       _ <- i3sumLast.join.flatMap(d => console.putStrLn(s"Sum of 10 recent prices for INSTRUMENT3 is $d"))
     } yield ()).exitCode
-
-trait Filter extends (Instrument => Boolean):
-  def and(another: Filter): Filter = Predicate(i => apply(i) && another(i))
-  def or(another: Filter): Filter = Predicate(i => apply(i) || another(i))
-
-case class Predicate(f: Instrument => Boolean) extends Filter:
-  override def apply(i: Instrument): Boolean = f(i)
-
-case class Name(name: String) extends Filter:
-  override def apply(i: Instrument): Boolean =
-    i.name == name
-
-case class Date(from: LocalDate = LocalDate.MIN, to: LocalDate = LocalDate.MAX) extends Filter:
-  override def apply(i: Instrument): Boolean = i.date.compareTo(from) >= 0 && i.date.compareTo(to) <= 0
-
-case object WorkingDays extends Filter:
-  val weekend = Set(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY)
-  override def apply(i: Instrument): Boolean = !weekend.contains(i.date.getDayOfWeek)
