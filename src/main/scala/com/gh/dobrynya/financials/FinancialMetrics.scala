3563664af99ec92f087e631a33a5fc4afa804972
Dmitry Dobrynin <dobrynya@inbox.ru>
2022-01-20 14:11:22 +0300

    Changed implementation of FinancialMetrics

--- src/main/scala/com/gh/dobrynya/financials/FinancialMetrics.scala
+++ src/main/scala/com/gh/dobrynya/financials/FinancialMetrics.scala
@@ -1,15 +1,16 @@
 package com.gh.dobrynya.financials
 
 import zio.*
+import clock.*
+import console.*
 import blocking.Blocking
 import zio.stream.*
 import java.nio.file.Paths
-import java.time.{DayOfWeek, LocalDate, Month}
+import java.time.{DayOfWeek, Duration, LocalDate, Month}
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 import scala.collection.immutable.SortedSet
 import scala.util.Try
-import java.time.LocalDate
 import java.time.Month.NOVEMBER
 
 extension (f: Instrument => Boolean)
@@ -19,10 +20,32 @@ extension (f: Instrument => Boolean)
 case class Instrument(name: String, date: LocalDate, price: Double)
 
 object Instrument:
-  def parse(s: String) = Try {
+  def parse(s: String): Option[Instrument] = Try {
     val p = s.split(",")
     Instrument(p(0), LocalDate.parse(p(1), DateTimeFormatter.ofPattern("dd-MMM-uuuu", Locale.US)), p(2).toDouble)
   }.toOption
+
+trait Indicator[T] extends (Instrument => UIO[Unit]) :
+  self =>
+  def value: T = ???
+  def filter(predicate: Instrument => Boolean): Indicator[T] =
+    new Indicator[T] :
+      override def value: T = self.value
+      override def apply(inst: Instrument) = self(inst).when(predicate(inst))
+  def +(that: Indicator[?]): Indicator[Any] = (v1: Instrument) => self.apply(v1) *> that(v1)
+
+class Avg(private var state: (Double, Int) = (0, 0)) extends Indicator[Double] :
+  override def apply(v1: Instrument) = UIO {
+    state = (state._1 + v1.price, state._2 + 1)
+  }
+  override def value: Double = state._1 / state._2
+
+class SumOfLast(n: Int = 10, private var state: SortedSet[Instrument] = SortedSet()(Ordering.by(_.date))) extends Indicator[Double] :
+  override def apply(v1: Instrument) = UIO {
+    val res = state + v1
+    state = if (res.size > n) res.drop(1) else res
+  }
+  override def value = state.map(_.price).sum
 
 def name(name: String) = (i: Instrument) => i.name == name
 
@@ -32,38 +55,28 @@ def date(from: LocalDate = LocalDate.MIN, to: LocalDate = LocalDate.MAX) =
 val workingDays: (Instrument => Boolean) =
   (i: Instrument) => i.date.getDayOfWeek != DayOfWeek.SUNDAY && i.date.getDayOfWeek != DayOfWeek.SATURDAY
 
-object FinancialMetrics extends App:
-  val raw =
-    ZStream.fromFile(Paths.get("instruments.txt")) >>> ZTransducer.utfDecode >>> ZTransducer.splitLines
-
+object FinancialMetrics extends App :
   val instruments: ZStream[Blocking, Throwable, Instrument] =
-    raw.map(Instrument.parse).collectSome
+    (ZStream.fromFile(Paths.get("instruments.txt")) >>> ZTransducer.utfDecode >>> ZTransducer.splitLines)
+      .map(Instrument.parse).collectSome
       .filter(date(to = LocalDate.of(2014, NOVEMBER, 19)) and workingDays)
+  val i1mean = new Avg().filter(name("INSTRUMENT1"))
+  val i2mean4november2014 = new Avg().filter(name("INSTRUMENT2") and
+    date(LocalDate.of(2014, NOVEMBER, 1), LocalDate.of(2014, NOVEMBER, 30)))
+  val i3sumLast = new SumOfLast().filter(name("INSTRUMENT3"))
 
-  val mean: Sink[Nothing, Instrument, Nothing, Double] = ZSink.foldLeft((0.0, 0)) { (s, elem: Instrument) =>
-    (s._1 + elem.price, s._2 + 1)
-  }.map(s => s._1 / s._2)
+  override def run(args: List[String]) = time {
+    instruments.repeat(Schedule.recurs(10)).foreach(i1mean + i2mean4november2014 + i3sumLast) *>
+      console.putStrLn("Completed reading file") *>
+      console.putStrLn(s"Mean for INSTRUMENT1 is ${i1mean.value}") *>
+      console.putStrLn(s"Mean for INSTRUMENT2 for November 2014 is ${i2mean4november2014.value}") *>
+      console.putStrLn(s"Sum of 10 recent prices for INSTRUMENT3 is ${i3sumLast.value}")
+  }.exitCode
 
-  def sumLast(n: Int) =
-    ZSink.foldLeft(SortedSet[Instrument]()(Ordering.by(_.date))) { (s, i: Instrument) =>
-      val result = s + i
-      if (result.size > n) result.drop(1) else result
-    }.map(_.map(_.price).sum)
-
-  def makeCalculation[R](hub: Hub[Instrument], sink: Sink[Nothing, Instrument, Nothing, R], filter: Instrument => Boolean): URIO[Any, Fiber.Runtime[Nothing, R]] =
-    ZStream.fromHub(hub).filter(filter).run(sink).fork
-
-  override def run(args: List[String]) =
-    (for {
-      hub <- ZHub.unbounded[Instrument]
-      i1mean <- makeCalculation(hub, mean, name("INSTRUMENT1"))
-      i2mean4november2014 <- makeCalculation(hub, mean, name("INSTRUMENT2") and
-        date(LocalDate.of(2014, NOVEMBER, 1), LocalDate.of(2014, NOVEMBER, 30)))
-      i3sumLast <- makeCalculation(hub, sumLast(10), name("INSTRUMENT3"))
-      _ <- instruments.foreach(hub.publish)
-      _ <- console.putStrLn("Completed reading file")
-      _ <- hub.shutdown
-      _ <- i1mean.join.flatMap(d => console.putStrLn(s"Mean for INSTRUMENT1 is $d"))
-      _ <- i2mean4november2014.join.flatMap(d => console.putStrLn(s"Mean for INSTRUMENT2 for November 2014 is $d"))
-      _ <- i3sumLast.join.flatMap(d => console.putStrLn(s"Sum of 10 recent prices for INSTRUMENT3 is $d"))
-    } yield ()).exitCode
+def time[R, E, A](effect: ZIO[R, E, A]): ZIO[R & Console & Clock, E, A] =
+  for {
+    start <- currentDateTime.orDie
+    result <- effect
+    end <- currentDateTime.orDie
+    _ <- putStrLn(s"Evaluating effect took ${Duration.between(start, end).toMillis} ms").orDie
+  } yield result
